fa9968b5e4fd34798646cb4e3a97009e8fe339f3
Nicholai Aldea <nicholairedaldea@gmail.com>
2026-09-10 20:48:07 +0800

    Fix output formatting for total cost display

--- code_challenge3.py
+++ code_challenge3.py
@@ -13,10 +13,10 @@ base_cost= (weight * 2.50) + (distance * 0.15)
 if weight <= 2.0 and distance <=100.0 and is_Express == 'False' and is_International == 'False':
 	print('Free Shipping')
 elif is_International == 'True' and is_Express == 'True':
-	print('Total=', (base_cost * 1.40) + 50)
+	print('Total=$', (base_cost * 1.40) + 50)
 elif is_Express == 'True' or is_International == 'True' and weight > 20:
-	print('Total=', (base_cost * 1.20) + 25)
+	print('Total=$', (base_cost * 1.20) + 25)
 elif weight > 30 or distance > 1000:
-	print('Total=', base_cost + 30)
+	print('Total=$', base_cost + 30)
 else:
-	print('Total=', base_cost)
+	print('Total=$', base_cost)
